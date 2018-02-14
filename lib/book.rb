a4b78b1687488784116444a4fb8b517cdeceeed3
Kaitlin Ryan <kaitlinaryan1@gmail.com>
2018-02-14 17:09:01 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,6 +1,6 @@
 class Book
-  attr_accessor :author, :page_count, :genre
-  attr_reader :title
+  attr_accessor :author, :page_count,
+  attr_reader :title, :genre
 
 GENRES = []
 
@@ -11,5 +11,13 @@ GENRES = []
   def turn_page
     puts "Flipping the page...wow, you read fast!"
   end
-
+def genre=(genre)
+  @genre = genre
+  GENRES << genre
 end
+end
+
+
+
+
+learn spec/01_book_spec.rb
